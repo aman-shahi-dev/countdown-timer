d9bca05a1fdd8f239de39535b471ac03aa92c0b7
Aman Shahi <amanshahidev@gmail.com>
2025-06-18 13:27:31 +0530

    Update script.js
    added sound effects, start/stop buttons add kiye aur unko logic likha, multiple intervals na chale, usko liye bhi logic likha

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+const tickingAudio = document.getElementById('ticking')
+
+const stopAudio = document.getElementById('stop')
+
+
+
+const countdownStartBtn = document.getElementById('countdownStartBtn')
+
 const startBtn = document.getElementById('startBtn')
+
+const stopBtn = document.getElementById('stopBtn')
+
+const resetBtn = document.getElementById('resetBtn')
 
 const inputTime = document.getElementById('inputTime')
 
 const result = document.getElementById('result')
+
+let timer = null
+
+let isRunning = false
+
+let valueInSeconds = 0
+
+startBtn.disabled = true
 
 function startTimer(){
 
     result.innerText = `Clock's ticking ⏰`;
     result.style.fontSize='1.6rem'
     result.style.zIndex = '1111';
-
-    let valueInSeconds = Number(inputTime.value)
+    
+    valueInSeconds = Number(inputTime.value)
     
     if ( isNaN(valueInSeconds) || !Number.isFinite(valueInSeconds) || inputTime.value.trim() === "" ) {
         result.innerText = `Please enter a valid time`; 
@@ -23,10 +43,16 @@ function startTimer(){
         result.innerText = `Please enter a time > 0`;
         result.style.zIndex = '1111';
         result.style.fontSize='1.2rem'
-        return // agar yeh nahi likhoge toh negative aur zero ka if conditions dono chalega aur dono statements print honge
+        return
     }
 
-    const timer = setInterval( function(){
+    clearInterval(timer)
+
+    countdownStartBtn.disabled = true 
+    
+    timer = setInterval( function(){
+
+        isRunning = true
 
         valueInSeconds-=1
 
@@ -34,10 +60,14 @@ function startTimer(){
         result.style.fontSize = '5rem'
         result.innerText = valueInSeconds
 
-        if ( valueInSeconds <=0 ){
+        if ( valueInSeconds > 0 ){
+            tickingAudio.play()
+        } else {
             result.style.fontSize = '2rem'
             result.style.zIndex = '1111';
             result.innerText = `Time's Up ⏰`
+            stopAudio.play()
+            isRunning = false
             clearInterval(timer)
         }
 
@@ -45,4 +75,64 @@ function startTimer(){
 
 }
 
-startBtn.addEventListener('click',startTimer)
+countdownStartBtn.addEventListener('click',startTimer)
+
+startBtn.addEventListener('click', () => {
+    if( isRunning === false){
+
+        if (valueInSeconds <= 0) {
+            result.innerText = `Please start a new timer first ⏱️`
+            result.style.fontSize = '1rem'
+            result.style.zIndex = '1111'
+            return
+        }
+
+        isRunning = true
+
+        clearInterval(timer); 
+
+        startBtn.disabled = true
+
+        timer = setInterval( () => {
+            
+            valueInSeconds-=1
+
+            result.style.zIndex = '1111';
+            result.style.fontSize = '5rem'
+            result.innerText = valueInSeconds
+
+            if ( valueInSeconds > 0 ){
+                tickingAudio.play()
+            } else {
+                result.style.fontSize = '2rem'
+                result.style.zIndex = '1111';
+                result.innerText = `Time's Up ⏰`
+                stopAudio.play()
+                isRunning = false
+                clearInterval(timer)
+            }
+
+        } , 1 * 1000)
+    }
+})
+
+stopBtn.addEventListener( 'click', () => {
+    if(valueInSeconds > 0){
+        result.style.zIndex = '1111'
+        result.innerText = `Paused ⏸️ (${valueInSeconds}s left)`
+        result.style.fontSize = '1.5rem'
+        clearInterval(timer)
+        isRunning = false
+        startBtn.disabled = false
+    }
+
+})
+
+resetBtn.addEventListener('click', () => {
+            result.style.zIndex = '-1111'
+            clearInterval(timer)
+            inputTime.value = ''
+            isRunning = false
+            countdownStartBtn.disabled = false 
+            startBtn.disabled = true
+})
